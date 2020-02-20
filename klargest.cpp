c0468c65e909114ff99348da5003a6252d3d2733
AkshaySB2000 <53827046+AkshaySB2000@users.noreply.github.com>
2020-02-20 19:12:04 +0530

    Add files via upload

--- klargest.cpp
+++ klargest.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-	int a[20],n,i,j,k,temp;
+	int a[20],n,i,j,k,temp,min;
 	cout<<"Enter the number of elements\n";
 	cin>>n;
 	cout<<"Enter the elements\n";
@@ -13,18 +13,18 @@ int main()
 	cin>>k;
 	for(i=0;i<n-1;i++)
 	{
-		for(j=0;j<n-i-1;j++)
+		min = i;
+		for(j=i+1;j<n;j++)
 		{
-			if(a[j]<a[j+1])
-			{
-				temp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = temp;
-			}
+			if(a[j]<a[min])
+				min = j;
 		}
+		temp = a[i];
+		a[i] = a[min];
+		a[min] = temp;
 	}
-	cout<<"\nThe 'k' largest elements in the array are\n";
-	for(i=0;i<k;i++)	
+	cout<<"\nThe "<<k<<" largest elements in the array are\n";
+	for(i=n-1;i>n-k-1;i--)	
 		cout<<a[i]<<" ";
 	cout<<endl;
 	return 0;

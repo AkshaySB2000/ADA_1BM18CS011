8750f4ae2c5483195722663b4c6bbdc6d32e0345
AkshaySB2000 <53827046+AkshaySB2000@users.noreply.github.com>
2020-02-20 19:17:36 +0530

    Add files via upload

---
+++ ssort.cpp
@@ -0,0 +1,35 @@
+#include<algorithm>
+#include<iostream>
+#include<ctime>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a[20],n,i,j,temp,min;
+	clock_t time;
+	cout<<"Enter the number of elements\n";
+	cin>>n;
+	cout<<"Enter the elements\n";
+	for(i=0;i<n;i++)
+		cin>>a[i];
+	time = clock();
+	for(i=0;i<n-1;i++)
+	{
+		min = i;
+		for(j=i+1;j<n;j++)
+		{
+			if(a[j]<a[min])
+				min = j;
+		}
+		temp = a[i];
+		a[i] = a[min];
+		a[min] = temp;
+	}
+	time = clock() - time;
+	cout<<"\nThe time required for selection sort is "<<fixed<<setprecision(10)<<(float)time/CLOCKS_PER_SEC<<"\n";
+	cout<<"\nThe sorted array is\n";
+	for(i=0;i<n;i++)
+		cout<<a[i]<<" ";
+	cout<<endl;  	
+	return 0;
+}

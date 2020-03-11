bfdc289663cf556a573147a3934c30adeb6f4b74
AkshaySB2000 <53827046+AkshaySB2000@users.noreply.github.com>
2020-03-11 22:37:10 +0530

    Add files via upload

--- dfs.cpp
+++ dfs.cpp
@@ -2,6 +2,27 @@
 #include<iostream>
 using namespace std;
 int a[20][20],n,visited[20],s[20],top=-1,c=1,flag=0;
+void dfs(int v);
+int main()
+{
+	int v,i,j;
+	cout<<"Enter the number of vertices\n";
+	cin>>n;
+	cout<<"Enter the adjacency matrix\n";
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+			cin>>a[i][j];
+	}
+	for(i=1;i<=n;i++)
+		visited[i]=0;
+	dfs(1);
+	if(c==n)
+		cout<<"\nThe graph is a connected graph\n";
+	else
+		cout<<"\nThe graph is not a connected graph\n";
+	return 0;
+}
 void dfs(int v)
 {
 	int i,j;
@@ -30,25 +51,5 @@ void dfs(int v)
 		if(flag==0)
 			top--;
 	}
-}
-int main()
-{
-	int v,i,j;
-	cout<<"Enter the number of vertices\n";
-	cin>>n;
-	cout<<"Enter the adjacency matrix\n";
-	for(i=1;i<=n;i++)
-	{
-		for(j=1;j<=n;j++)
-			cin>>a[i][j];
-	}
-	for(i=1;i<=n;i++)
-		visited[i]=0;
-	dfs(1);
-	if(c==n)
-		cout<<"\nThe graph is a connected graph\n";
-	else
-		cout<<"\nThe graph is not a connected graph\n";
-	return 0;
 }
 
